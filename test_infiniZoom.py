ca716110b8e2dcd7f0759b059f5aa612f6efda7f
amcauley <amcauley@ucla.edu>
2014-12-21 03:19:14 -0800

    Initial Commit
    Initial commit. Project plan is to create a GIMP plugin that creates
    infinitely looping/zooming GIFs. Currently plugin only creates a new
    layer zoomed into a specified coordinate.

---
+++ test_infiniZoom.py
@@ -0,0 +1,77 @@
+'''API notes from http://www.gimp.org/docs/python/ .
+   This file should be copied to Program Files\GIMP 2\lib\gimp\2.0\plug-ins.'''
+
+from gimpfu import *
+
+def infiniZoom(img, layer) :
+    print("Starting infiniZoom")
+    
+    oldWidth = layer.width
+    oldHeight = layer.height
+    
+    '''location (old coords) that will be at the origin of the new layer. no need to quantize here, will happen later'''
+    newXOrg = 20
+    newYOrg = 100
+    
+    scaleFactor = 2
+    
+    newWidth = int(oldWidth*scaleFactor)
+    newHeight = int(oldHeight*scaleFactor)
+    newX = int(newXOrg*scaleFactor)
+    newY = int(newYOrg*scaleFactor)
+    
+    print("(newX, newY) = " + str((newX,newY)))
+    
+    layerCnt = 1
+    layerName = "layer " + str(layerCnt)
+    
+    print("Creating new layer: " + layerName)
+    currLayer = layer.copy()
+    currLayer.name = layerName
+    
+    print("Adding " + str(layerName) + " to img")
+    img.add_layer(currLayer, 0) 
+   
+    #scale the entire layer
+    print("Scaling Layer")
+    '''scale(h, w, oldin) scales the layer to (w, h), using the specified origin (local or image).'''
+    currLayer.scale(newWidth, newHeight, 0)       
+
+    #move the upper left corner of the scaled image so that the desired region of the scaled layer is visible
+    print("Translating Layer")
+    '''Moves the layer to (x, y) relative to its current position.'''
+    ##currLayer.translate(-oldWidth, -oldHeight)    
+    currLayer.translate(-newX, -newY)
+    
+    #resize the layer so it matches original layer. this is the equivalent of Layer->"Layer Boundary Size" option
+    print("Resizing Layer")
+    '''resize(w, h, x, y) resizes the layer to (w, h), positioning the original contents at (x,y).'''    
+    ##currLayer.resize(oldWidth, oldHeight, -oldWidth, -oldHeight)
+    currLayer.resize(oldWidth, oldHeight, -newX, -newY)
+    
+    print("Done!")
+
+'''Disable the rest of this file (register and main) for in-console GIMP debugging, then, in the python-fu console:
+    import sys
+    sys.path=[gimp.directory+'/plug-ins']+sys.path
+    import test_infiniZoom
+    test_infiniZoom.infiniZoom(gimp.image_list()[0],gimp.image_list()[0].active_layer)
+    
+    To reload module: reload(test_infiniZoom)
+'''
+ 
+'''register(
+    "python_fu_test_infiniZoom",
+    "InfiniZoom",
+    "Infinite Zoom GIF Generator",
+    "AGM",
+    "Open source (BSD 3-clause license)",
+    "2014",
+    "<Image>/Filters/Test/Draw Map v1",
+    "RGB, RGB*",
+    [],
+    [],
+    infiniZoom)
+
+main()
+'''
